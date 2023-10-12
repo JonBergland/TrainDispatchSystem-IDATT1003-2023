b0c79e6fbf14f230071b1a4b9bc20df522c78824
Jon Bergland <jobergl@stud.ntnu.no>
2023-10-12 21:26:25 +0200

    printer ut objektene i listen table med userInterface

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -10,12 +10,13 @@ public class TrainDispatchApp {
 
   public static void main(String[] args) {
     trainDeparture[] table = new trainDeparture[2];
-    table[0] = new trainDeparture(LocalTime.of(12, 15), "Linje 1", 601, "Frognerseteren", null, -1);
-    table[1] = new trainDeparture(LocalTime.of(15, 30), "Linje 2", 305, "Sognsvann", null, -1);
+    table[0] = new trainDeparture(LocalTime.of(12, 15), "Linje 1", 601, "Frognerseteren", LocalTime.of(0, 0), -1);
+    table[1] = new trainDeparture(LocalTime.of(15, 30), "Linje 2", 305, "Sognsvann", LocalTime.of(0, 0), -1);
 
 
     for(trainDeparture i : table){
-      System.out.println(i.getDepartureTime());
+      userInterface userInterface = new userInterface(i);
+      userInterface.printTrainDeparture();
     }
   }
 
