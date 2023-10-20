2e340d85ea0186a5956c283f5184c48b555c41e5
Jon Bergland <jobergl@stud.ntnu.no>
2023-10-20 17:05:42 +0200

    lagde en klasse som tar vare på tabell listen

---
+++ src/main/java/edu/ntnu/stud/Table.java
@@ -0,0 +1,11 @@
+package edu.ntnu.stud;
+
+import java.util.ArrayList;
+
+public class Table{
+    private ArrayList<TrainDeparture> table = new ArrayList<>();
+
+    public ArrayList<TrainDeparture> getTable() {
+        return table;
+    }
+}
