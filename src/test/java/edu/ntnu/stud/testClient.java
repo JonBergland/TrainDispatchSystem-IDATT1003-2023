7737989b55673f0dfe169b03fe7b06f990541c7a
Jon Bergland <jobergl@stud.ntnu.no>
2023-10-20 17:06:45 +0200

    lagde en testklient fil for testing av metoder

---
+++ src/test/java/edu/ntnu/stud/testClient.java
@@ -0,0 +1,15 @@
+package edu.ntnu.stud;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+class testClient {
+    final ArrayList<trainDeparture> table;
+    public testClient(ArrayList<trainDeparture> table){this.table = table;}
+
+    public boolean test1(){
+        boolean utskrift = true;
+
+        return utskrift;
+    }
+}
