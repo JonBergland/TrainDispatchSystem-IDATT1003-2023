d464f57a6bc2a12b8aa23c26cdd389a2016924b9
Jon Bergland <jobergl@stud.ntnu.no>
2023-10-24 11:51:55 +0200

    endret til å compare departureTime, ikke orginalDepartureTime

--- src/main/java/edu/ntnu/stud/sortByTime.java
+++ src/main/java/edu/ntnu/stud/sortByTime.java
@@ -2,9 +2,10 @@ package edu.ntnu.stud;
 
 import java.util.Comparator;
 
+//en klasse som skal sammenligne TrainDeparture objekter
 public class sortByTime implements Comparator<TrainDeparture> {
-    public int compare(TrainDeparture a, TrainDeparture b)
-    {
-        return a.getOriginalDepartureTime().compareTo(b.getOriginalDepartureTime());
+    public int compare(TrainDeparture a, TrainDeparture b){
+        //har en metode som sammenligner den reele avgangstiden med hverandre
+        return a.getDepartureTime().compareTo(b.getDepartureTime());
     }
 }
