02744a1cfe14c9e7a71f88534aeaca0acea05612
Jon Bergland <jobergl@stud.ntnu.no>
2023-11-15 23:24:11 +0100

    Refactor: Made sort-function use ArrayList instead of LinkedList

--- src/main/java/edu/ntnu/stud/SortByTime.java
+++ src/main/java/edu/ntnu/stud/SortByTime.java
@@ -6,13 +6,12 @@ import java.util.*;
 public class SortByTime {
   public static HashMap<Integer, TrainDeparture> sort(HashMap<Integer, TrainDeparture> map){
     List<Map.Entry<Integer, TrainDeparture> > list =
-        new LinkedList<>(map.entrySet());
+        new ArrayList<>(map.entrySet());
 
     list.sort(Comparator.comparing(a -> a.getValue().getOriginalDepartureTime()));
 
     HashMap<Integer, TrainDeparture> newHashMap = new LinkedHashMap<Integer, TrainDeparture>();
     for (Map.Entry<Integer, TrainDeparture> hashMap : list) {
-      System.out.println(hashMap.getKey());
       newHashMap.put(hashMap.getKey(), hashMap.getValue());
     }
     return newHashMap;
