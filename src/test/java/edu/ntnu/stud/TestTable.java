9aeec6b3773a3144ceded4f99ed96558cbe22b65
Jon Bergland <jobergl@stud.ntnu.no>
2023-11-20 22:07:54 +0100

    Feat: Made get() method tests for table

--- src/test/java/edu/ntnu/stud/TestTable.java
+++ src/test/java/edu/ntnu/stud/TestTable.java
@@ -2,6 +2,7 @@ package edu.ntnu.stud;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
@@ -10,9 +11,9 @@ import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("Table_TrainDeparture_Register-class")
+@DisplayName("Table-class")
 public class TestTable {
-  Table table = new Table();
+  Table tableTest = new Table();
   int trainNumber1;
   TrainDeparture trainDeparture1;
   int trainNumber2;
@@ -33,30 +34,48 @@ public class TestTable {
     trainNumber4 = 406;
     trainDeparture4 = new TrainDeparture(LocalTime.of(10, 40), "L4", "Lillehammer", -1);
 
-    table.getHashMap().put(trainNumber1, trainDeparture1);
-    table.getHashMap().put(trainNumber2, trainDeparture2);
-    table.getHashMap().put(trainNumber3, trainDeparture3);
-    table.getHashMap().put(trainNumber4, trainDeparture4);
+    tableTest.getHashMap().put(trainNumber1, trainDeparture1);
+    tableTest.getHashMap().put(trainNumber2, trainDeparture2);
+    tableTest.getHashMap().put(trainNumber3, trainDeparture3);
+    tableTest.getHashMap().put(trainNumber4, trainDeparture4);
   }
+  @Nested
+  @DisplayName("get() methods")
+  class tableGetMethods {
 
-  @DisplayName("Test of getTrainByDestination")
-  @Test
-  void testGetTrainByDestination() {
-    String destination = "Oslo";
+    @DisplayName("Test of getHashMap")
+    @Test
+    void testGetHashMap() {
+      HashMap<Integer, TrainDeparture> newHashMap = new HashMap<>();
+      newHashMap.put(trainNumber1, trainDeparture1);
+      newHashMap.put(trainNumber2, trainDeparture2);
+      newHashMap.put(trainNumber3, trainDeparture3);
+      newHashMap.put(trainNumber4, trainDeparture4);
 
-    HashMap<Integer, TrainDeparture> destinationList = new HashMap<>();
-    destinationList.put(trainNumber1, trainDeparture1);
-    destinationList.put(trainNumber3, trainDeparture3);
+      assertEquals(newHashMap, tableTest.getHashMap());
+    }
 
-    assertEquals(destinationList, table.getTrainByDestination(destination));
-  }
+    @DisplayName("Test of getTrainByDestination")
+    @Test
+    void testGetTrainByDestination() {
+      String destination = "Oslo";
+      HashMap<Integer, TrainDeparture> destinationList = new HashMap<>();
+      destinationList.put(trainNumber1, trainDeparture1);
+      destinationList.put(trainNumber3, trainDeparture3);
 
-  @DisplayName("Test of getUniqueDestinations")
-  @Test
-  void testGetUniqueDestinations() {
-    HashSet<String> uniqueDestinations = new HashSet<>();
-    uniqueDestinations.add("Oslo");
-    uniqueDestinations.add("")
+      assertEquals(destinationList, tableTest.getTrainByDestination(destination));
+    }
+
+    @DisplayName("Test of getUniqueDestinations")
+    @Test
+    void testGetUniqueDestinations() {
+      HashSet<String> uniqueDestinations = new HashSet<>();
+      uniqueDestinations.add("Oslo");
+      uniqueDestinations.add("Hamar");
+      uniqueDestinations.add("Lillehammer");
+
+      assertEquals(uniqueDestinations, tableTest.getUniqueDestinationList());
+    }
   }
 }
 
