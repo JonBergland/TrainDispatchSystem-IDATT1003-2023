8e33373bf23550804a7331f6c429d91bcde250df
Jon Bergland <jobergl@stud.ntnu.no>
2023-11-27 22:13:54 +0100

    Feat: Make a exception method without arguments

--- src/main/java/edu/ntnu/stud/Exceptions/TableAddException.java
+++ src/main/java/edu/ntnu/stud/Exceptions/TableAddException.java
@@ -4,4 +4,8 @@ public class TableAddException extends Exception{
   public TableAddException(String errorMessage) {
     super(errorMessage);
   }
+  public TableAddException() {
+    super("Something wrong happened during the creation of the train-departure");
+  }
 }
+
