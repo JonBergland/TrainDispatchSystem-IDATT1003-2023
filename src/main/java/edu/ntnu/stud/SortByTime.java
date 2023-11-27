2d36b5080a980d19646da87f61ed65a917508adc
Jon Bergland <jobergl@stud.ntnu.no>
2023-11-27 15:55:09 +0100

    Refactor: Optimize the sorting algorithm with Java Streams

--- src/main/java/edu/ntnu/stud/SortByTime.java
+++ src/main/java/edu/ntnu/stud/SortByTime.java
@@ -1,20 +1,30 @@
 package edu.ntnu.stud;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-//en klasse som skal sammenligne TrainDeparture objekter
 public class SortByTime {
+  /**
+   * Sorts a HashMap of TrainDeparture objects based on their original departure times.
+   * This method takes a HashMap with Integer keys and TrainDeparture values and sorts
+   * it in ascending order based on the original departure times of the TrainDeparture objects.
+   * The sorting is performed using Java Streams and a Comparator which compares based on the
+   * departure times of the TrainDepartures. The resulting sorted map is returned as a
+   * LinkedHashMap to maintain the order of the TrainDepartures.
+   *
+   * @param map The unsorted HashMap containing Integer keys and TrainDeparture values.
+   * @return A LinkedHashMap with the entries sorted by the original departure times.
+   *         If the original departure times are equal, the order of insertion is maintained.
+   */
   public static HashMap<Integer, TrainDeparture> sort(HashMap<Integer, TrainDeparture> map){
-    List<Map.Entry<Integer, TrainDeparture> > list =
-        new ArrayList<>(map.entrySet());
-
-    list.sort(Comparator.comparing(a -> a.getValue().getOriginalDepartureTime()));
-
-    HashMap<Integer, TrainDeparture> newHashMap = new LinkedHashMap<Integer, TrainDeparture>();
-    for (Map.Entry<Integer, TrainDeparture> hashMap : list) {
-      newHashMap.put(hashMap.getKey(), hashMap.getValue());
-    }
-    return newHashMap;
+    return map.entrySet().stream()
+        .sorted(Comparator.comparing(entry -> entry.getValue().getOriginalDepartureTime()))
+        .collect(Collectors.toMap(
+            Map.Entry::getKey,
+            Map.Entry::getValue,
+            (existing, replacement) -> existing,
+            LinkedHashMap::new
+        ));
   }
 
 }
