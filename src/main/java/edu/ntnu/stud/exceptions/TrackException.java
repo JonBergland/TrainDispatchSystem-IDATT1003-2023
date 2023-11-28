04eb9854fb2e5ec714be081e0af3fdf6886a53b8
Jon Bergland <jobergl@stud.ntnu.no>
2023-11-28 14:17:41 +0100

    Docs: Add Javadocs to methods

--- src/main/java/edu/ntnu/stud/exceptions/TrackException.java
+++ src/main/java/edu/ntnu/stud/exceptions/TrackException.java
@@ -1,10 +1,36 @@
 package edu.ntnu.stud.exceptions;
 
+/**
+ * Exception class for errors that occur in the updating of a Clock-object.
+ * <p>
+ * This exception is thrown when there is an issue during the updating of the LocalTime
+ * variable in the clock-object. It extends the standard {@code Exception}
+ * class.
+ * </p>
+ *
+ * @see Exception
+ */
 public class TrackException extends Exception {
+  /**
+   * Constructs a TrackException with a specified error message.
+   * <p>
+   * This constructor creates an instance of the exception with an errormessage
+   * providing details on why the exception is thrown
+   * </p>
+   *
+   * @param errorMessage      A String containing the details on why the exception
+   *                          is thrown
+   */
   public TrackException(String errorMessage) {
     super(errorMessage);
   }
-
+  /**
+   * Constructs a TrackException with predetermined error message.
+   * <p>
+   * This constructor creates an instance of the exception with an predetermined
+   * errormessage providing general details on why the exception is thrown
+   * </p>
+   */
   public TrackException() {
     super("The track was not accepted");
   }
