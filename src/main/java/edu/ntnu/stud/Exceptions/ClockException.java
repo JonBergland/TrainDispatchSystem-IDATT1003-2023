b8bfa890c331fa221a2edef3892a25783f67fb46
Jon Bergland <jobergl@stud.ntnu.no>
2023-11-28 13:40:47 +0100

    Docs: Add Javadocs to methods

--- src/main/java/edu/ntnu/stud/Exceptions/ClockException.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.stud.Exceptions;
-
-public class ClockException extends Exception{
-  public ClockException(String errorMessage) {
-    super(errorMessage);
-  }
-}
---
+++ src/main/java/edu/ntnu/stud/exceptions/ClockException.java
@@ -0,0 +1,27 @@
+package edu.ntnu.stud.exceptions;
+
+/**
+ * Exception class for errors that occur in the updating of a Clock-object.
+ * <p>
+ * This exception is thrown when there is an issue during the updating of the LocalTime
+ * variable in the clock-object. It extends the standard {@code Exception}
+ * class.
+ * </p>
+ *
+ * @see Exception
+ */
+public class ClockException extends Exception{
+  /**
+   * Constructs a ClockException with a specified error message.
+   * <p>
+   * This constructor creates an instance of the exception with an errormessage
+   * providing details on why the exception is thrown
+   * </p>
+   *
+   * @param errorMessage      A String containing the details on why the exception
+   *                          is thrown
+   */
+  public ClockException(String errorMessage) {
+    super(errorMessage);
+  }
+}
