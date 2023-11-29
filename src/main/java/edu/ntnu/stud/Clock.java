6d4bfd3aa29e91dd809e852de01ade737bb2a690
Jon Bergland <jobergl@stud.ntnu.no>
2023-11-29 14:08:00 +0100

    Docs: Add javadocs for methods

--- src/main/java/edu/ntnu/stud/Clock.java
+++ src/main/java/edu/ntnu/stud/Clock.java
@@ -1,49 +1,59 @@
 package edu.ntnu.stud;
 
-import edu.ntnu.stud.Exceptions.ClockException;
-
+import edu.ntnu.stud.exceptions.ClockException;
 import java.time.DateTimeException;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * Dette er klassen for klokka
+ * Represents a clock with LocalTime functionality.
+ * <p>
+ * The {@code Clock} class provides methods for initializing, retrieving, and updating
+ * the current time represented by a LocalTime object. It includes functionality to set
+ * a new time while ensuring it is valid and not earlier than the current time.
+ * </p>
+ *
+ * @see LocalTime
+ * @see DateTimeFormatter
+ * @see ClockException
  */
 public class Clock {
-  /**
-   * Objekts variabel
-   */
   private LocalTime clock;
 
   /**
-   * Initialize the Clock object with a LocalTime of 0
+   * Initialize the Clock object with a LocalTime of 0.
    */
   public Clock() { //når Clock blir initialisert settes den til 00:00
     this.clock = LocalTime.of(0, 0);
   }
 
   /**
-   * @return LocalTime        Gets the current time
+   * Gets the LocalTime representing the current time.
+   *
+   * @return The LocalTime object representing the current time.
    */
   public LocalTime getClock() { //en get-metode for å returnere klokkeslettet
     return clock;
   }
 
   /**
-   * @param time The time the new clock is set to be
+   * Sets the clock to a new time specified by the given string.
+   *
+   * @param newTime         The time to which the clock is set, provided as a string.
+   * @throws ClockException If the format of the new time is invalid or if the new time
+   *                        is before the current time, a ClockException is thrown.
    */
-  public void setClock(String newTime) throws ClockException { //en setmetode som setter parameteren lik klokkeslettet
-    LocalTime time = LocalTime.of(0,0);
+  public void setClock(String newTime) throws ClockException {
+    LocalTime time;
     try {
       time =
           LocalTime.parse(LocalTime.now().format(DateTimeFormatter.ofPattern(newTime)));
     } catch (DateTimeException e) {
-      throw new ClockException("The time format was not valid");
+      throw new ClockException("The format on the new time was not valid");
     }
-    if (time.isBefore(clock)){
+    if (time.isBefore(clock)) {
       throw new ClockException("The new time is before the old time");
-    }
-    else {
+    } else {
       this.clock = time;
     }
   }
