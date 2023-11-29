bd37daaa61b764be8c787be4ee599c1d9f49e24f
Jon Bergland <jobergl@stud.ntnu.no>
2023-11-29 13:43:18 +0100

    Docs: Make javadoc for SortByTime class

--- src/main/java/edu/ntnu/stud/SortByTime.java
+++ src/main/java/edu/ntnu/stud/SortByTime.java
@@ -1,8 +1,27 @@
 package edu.ntnu.stud;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Utility class for sorting a HashMap of TrainDeparture objects based on their original departure times.
+ * <p>
+ * The {@code SortByTime} class provides a static method to sort a HashMap with Integer keys and
+ * TrainDeparture values in ascending order based on the original departure times of the TrainDeparture objects.
+ * The sorting is performed using Java Streams and a Comparator which compares based on the
+ * departure times of the TrainDepartures. The resulting sorted map is returned as a LinkedHashMap to
+ * maintain the order of the TrainDepartures.
+ * </p>
+ *
+ * @see TrainDeparture
+ * @see java.util.HashMap
+ * @see java.util.LinkedHashMap
+ * @see java.util.Comparator
+ * @see java.util.stream.Collectors
+ */
 public class SortByTime {
   /**
    * Sorts a HashMap of TrainDeparture objects based on their original departure times.
@@ -16,7 +35,7 @@ public class SortByTime {
    * @return A LinkedHashMap with the entries sorted by the original departure times.
    *         If the original departure times are equal, the order of insertion is maintained.
    */
-  public static HashMap<Integer, TrainDeparture> sort(HashMap<Integer, TrainDeparture> map){
+  public static HashMap<Integer, TrainDeparture> sort(HashMap<Integer, TrainDeparture> map) {
     return map.entrySet().stream()
         .sorted(Comparator.comparing(entry -> entry.getValue().getOriginalDepartureTime()))
         .collect(Collectors.toMap(
