9f487fac62308d5fc50b02492bd1f76b9f01b77c
Jon Bergland <jobergl@stud.ntnu.no>
2023-12-04 16:22:27 +0100

    Refactor: Make tryInt public and return Integer

--- src/main/java/edu/ntnu/stud/Input.java
+++ src/main/java/edu/ntnu/stud/Input.java
@@ -1,8 +1,5 @@
 package edu.ntnu.stud;
 
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Input {
@@ -12,21 +9,20 @@ public class Input {
    * Checks if a given string can be parsed into an integer.
    * <p>
    * This method attempts to parse the specified string into an integer using {@code Integer.parseInt}.
-   * If the parsing is successful, the method returns {@code true}; otherwise, it returns {@code false}.
+   * If the parsing is successful, the method returns the parsed number; otherwise, it returns a dummy value.
    * </p>
    *
-   * @param number    The string to be checked for integer validity.
-   * @return {@code true} if the string can be parsed into an integer, {@code false} otherwise.
+   * @param number      The string to be checked for integer validity.
+   * @param dummyValue  The value that gets returned if number can't be parsed into an integer
+   * @return {@code number} if the string can be parsed into an integer, dummyValue otherwise.
    * @see Integer#parseInt(String)
    */
-  private boolean tryInt(String number) { //en metode for å sjekke om en streng er et tall (int)
-    boolean isInt = true;
+  public int tryInt(String number, int dummyValue) {
     try {
-      Integer.parseInt(number); //sjekker om strengen kan konverteres til tall
+      return Integer.parseInt(number);
     } catch (NumberFormatException e) {
-      isInt = false;
+      return dummyValue;
      }
-    return isInt;
   }
 
   /**
@@ -41,16 +37,12 @@ public class Input {
    * @param print         The message to prompt the user for input.
    * @param dummyValue    The default value returned in case of invalid input.
    * @return An integer input provided by the user or the specified dummyValue if the input is invalid.
-   * @see #tryInt(String)
+   * @see #tryInt(String, int)
    */
   public int intInput(String print, int dummyValue) {
-    System.out.println(print);
+    //System.out.println(print);
     String intInput = in.nextLine();
-    int output = dummyValue;
-    if (tryInt(intInput)) {
-      output = Integer.parseInt(intInput);
-    }
-    return output;
+    return tryInt(intInput, dummyValue);
   }
 
   /**
@@ -64,8 +56,8 @@ public class Input {
    * @param print   The message to prompt the user for a string input.
    * @return The string input provided by the user.
    */
-  public String stringInput(String print) { //en metode for en streng-input fra bruker
-    System.out.println(print); //skriver ut en egendefinert streng til bruker
-    return in.nextLine(); //returnerer bruker-inputet
+  public String stringInput(String print) {
+    System.out.println(print);
+    return in.nextLine();
   }
 }
