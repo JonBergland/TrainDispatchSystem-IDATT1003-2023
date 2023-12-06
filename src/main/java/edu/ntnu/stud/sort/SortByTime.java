0a314fd1ae74fb78d9ef87522dba1377495b8080
Jon Bergland <jobergl@stud.ntnu.no>
2023-12-06 13:53:34 +0100

    Docs: Change wording

--- src/main/java/edu/ntnu/stud/sort/SortByTime.java
+++ src/main/java/edu/ntnu/stud/sort/SortByTime.java
@@ -1,5 +1,6 @@
-package edu.ntnu.stud;
+package edu.ntnu.stud.sort;
 
+import edu.ntnu.stud.TrainDeparture;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -7,13 +8,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
- * Utility class for sorting a HashMap of TrainDeparture objects based on their original departure times.
+ * Utility class for sorting a HashMap of TrainDeparture objects based on
+ * their original departure times.
  * <p>
  * The {@code SortByTime} class provides a static method to sort a HashMap with Integer keys and
- * TrainDeparture values in ascending order based on the original departure times of the TrainDeparture objects.
- * The sorting is performed using Java Streams and a Comparator which compares based on the
- * departure times of the TrainDepartures. The resulting sorted map is returned as a LinkedHashMap to
- * maintain the order of the TrainDepartures.
+ * TrainDeparture values in ascending order based on the original departure times
+ * of the TrainDeparture objects.
  * </p>
  *
  * @see TrainDeparture
@@ -32,6 +32,7 @@ public class SortByTime {
    * departure times of the TrainDepartures. The resulting sorted map is returned as a
    * LinkedHashMap to maintain the order of the TrainDepartures.
    * </p>
+   *
    * @param map   The unsorted HashMap containing Integer keys and TrainDeparture values.
    * @return      A LinkedHashMap with the entries sorted by the original departure times.
    *              If the original departure times are equal, the order of insertion is maintained.
@@ -42,6 +43,7 @@ public class SortByTime {
         .collect(Collectors.toMap(
             Map.Entry::getKey,
             Map.Entry::getValue,
+            //if there are two entries with the same key, the existing entry is kept
             (existing, replacement) -> existing,
             LinkedHashMap::new
         ));
