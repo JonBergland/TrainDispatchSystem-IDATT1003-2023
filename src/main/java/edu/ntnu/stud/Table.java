fce07a75d05da802556bcddead2e0ee70c1ff62a
Jon Bergland <jobergl@stud.ntnu.no>
2023-12-07 11:07:15 +0100

    Docs: Add javadoc to remove-method

--- src/main/java/edu/ntnu/stud/Table.java
+++ src/main/java/edu/ntnu/stud/Table.java
@@ -14,9 +14,9 @@ import java.util.Map;
 /**
  * Represents a table of TrainDeparture-objects indexed by train numbers.
  * <p>
- *   The class contains a HashMap with Integer-keys (train numbers) and
- *   TrainDeparture-values. This class works as a container for managing and
- *   organizing TrainDeparture-objects.
+ * The class contains a HashMap with Integer-keys (train numbers) and
+ * TrainDeparture-values. This class works as a container for managing and
+ * organizing TrainDeparture-objects.
  * </p>
  */
 public class Table {
@@ -35,7 +35,8 @@ public class Table {
    *
    * @param oldHashMap The existing TrainDeparture object that is copied
    */
-  public Table(HashMap<Integer, TrainDeparture> oldHashMap) throws TrainDepartureConstructorException {
+  public Table(HashMap<Integer, TrainDeparture> oldHashMap)
+      throws TrainDepartureConstructorException {
     HashMap<Integer, TrainDeparture> newHashMap = new LinkedHashMap<>();
     for (Map.Entry<Integer, TrainDeparture> entry : oldHashMap.entrySet()) {
       newHashMap.put(entry.getKey(), new TrainDeparture(entry.getValue()));
@@ -45,7 +46,9 @@ public class Table {
 
   /**
    * Gets the list over trainDepartures.
+   * <p>
    * This method returns the HashMap in the Table-class
+   * </p>
    *
    * @return The Hashmap with all the TrainDeparture-objects
    */
@@ -55,12 +58,14 @@ public class Table {
 
   /**
    * Gets the TrainDeparture objects with a specific destination.
+   * <p>
    * This method iterates through all the entry-sets in the HashMap
    * variable in Table and returns all the entries with the specific
    * destination as a new HashMap
+   * </p>
    *
-   * @param destination                         The specific destination that the method
-   *                                            searches the HashMap for
+   * @param destination The specific destination that the method
+   *                    searches the HashMap for
    * @return destinationHashMap
    */
   public HashMap<Integer, TrainDeparture> getTrainByDestination(String destination) {
@@ -75,6 +80,10 @@ public class Table {
 
   /**
    * Gets all the unique destinations in Table.
+   * <p>
+   * This method loops through the HashMap in Table and adds the destinations to a
+   * hashset
+   * </p>
    *
    * @return uniqueDestinations
    */
@@ -92,11 +101,13 @@ public class Table {
 
   /**
    * Retrieves a TrainDeparture object based on a given Integer-key (trainNumber) from the HashMap.
+   * <p>
    * This method takes a given Integer-key and returns the associated TrainDeparture object.
    * If an associated TrainDeparture object doesn't exist, this method returns null.
+   * </p>
    *
-   * @param trainNumber               The key which this method gets the associated TrainDeparture
-   *                                  object for.
+   * @param trainNumber The key which this method gets the associated TrainDeparture
+   *                    object for.
    * @return The TrainDeparture object or null if the trainNumber has no associated TrainDeparture
    */
   public TrainDeparture getTrainByTrainNumber(int trainNumber) {
@@ -105,13 +116,15 @@ public class Table {
 
   /**
    * Adds an Integer-key (trainNumber) and a TrainDeparture-value (trainDeparture)
-   * to the Hashmap variable in Table. This method takes a trainNumber (which represents
+   * to the Hashmap variable in Table.
+   * <p>
+   * This method takes a trainNumber (which represents
    * the key in the HashMap) and a TrainDeparture object (which represents the value in
    * the HashMap).
+   * </p>
    *
-   * @param trainNumber               The unique train number that is set as the key in the hash map
-   * @param trainDeparture            The trainDeparture entity that the train number belongs to
-   *
+   * @param trainNumber    The unique train number that is set as the key in the hash map
+   * @param trainDeparture The trainDeparture entity that the train number belongs to
    * @throws IllegalArgumentException Throws Exception if the trainNumber
    *                                  already exists in the register
    */
@@ -133,11 +146,13 @@ public class Table {
 
   /**
    * Sets track to a specific train-departure in the HashMap.
+   * <p>
    * This method designates a track to the train-departure corresponding
    * to the given trainNumber
+   * </p>
    *
-   * @param trainNumber         The trainDeparture's trainNumber
-   * @param track               The track you want to assign
+   * @param trainNumber The trainDeparture's trainNumber
+   * @param track       The track you want to assign
    */
   public boolean setTrackToTrain(int trainNumber, int track) throws TrackException {
     return this.hashMap.get(trainNumber).setTrack(track);
@@ -145,11 +160,13 @@ public class Table {
 
   /**
    * Sets delay to a specific train-departure in the HashMap.
+   * <p>
    * This method sets a delay to the train-departure corresponding
    * to the given trainNumber.
+   * </p>
    *
-   * @param trainNumber         The trainDeparture's trainNumber
-   * @param delayInMinutes      The delay you want to assign
+   * @param trainNumber    The trainDeparture's trainNumber
+   * @param delayInMinutes The delay you want to assign
    */
   public boolean setDelayToTrain(int trainNumber, int delayInMinutes) throws DelayException {
     if (this.hashMap.get(trainNumber) != null) {
@@ -162,23 +179,33 @@ public class Table {
 
   /**
    * Removes trainDeparture objects that is before the specified time.
+   * <p>
    * This method iterates through all the entries in the hashmap and removes
    * those where the TrainDeparture objects have an earlier departure than
    * the specified time
+   * </p>
    *
-   * @param time            The LocalTime variable which marks a threshold time.
-   *                        All TrainDeparture-object with an earlier departure than
-   *                        time, gets removed
+   * @param time The LocalTime variable which marks a threshold time.
+   *             All TrainDeparture-object with an earlier departure than
+   *             time, gets removed
    */
   public void removeTrainDepartureBeforeTime(LocalTime time) {
     hashMap.entrySet().removeIf(map -> map.getValue().getDepartureTime().isBefore(time));
   }
 
+  /**
+   * Removes the train departure value that correspond with the
+   * train number key in the HashMap in Table.
+   *
+   * @param trainNumber The train number key corresponding with the train departure value
+   *                    you want to remove from the HashMap in table
+   * @return a boolean representing if the operation was successful or not
+   */
   public boolean remove(int trainNumber) {
     if (hashMap.get(trainNumber) != null) {
       hashMap.remove(trainNumber);
       return true;
-    } else{
+    } else {
       return false;
     }
   }
